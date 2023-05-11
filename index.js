2ec7981b2fb4aaa86ec3f9e152630671e4957a61
ZankSRWW23 <sarawut.p64@rsu.ac.th>
2023-05-11 22:41:29 +0700

    init commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT;
 const db = mysql.createConnection(process.env.DATABASE_URL);
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
